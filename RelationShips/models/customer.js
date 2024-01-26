8095c3f2df082e4d07cd5cd73b665173e7254c0c
TejwantKaur <kaurtejwant998@gmail.com>
2024-01-26 22:41:46 +0530

    customer

--- RelationShips/models/customer.js
+++ RelationShips/models/customer.js
@@ -9,12 +9,35 @@ async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
 }
 
+const orderSchema = new Schema({
+    item: String,
+    price: Number
+});
+
+const customerSchema = new Schema({
+    name: String,
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Order"
+        }
+    ]
+})
+
 const Order = mongoose.model("Order",orderSchema);
-const addOrders = async() => {
-    let res = await Order.insertMany([
-        { item: "Samosa", price: 12 } ,
-        { item: "Chips", price: 10 },
-        { item: "chocolate", price: 40 }
-    ]);
-    console.log(res);
+const Customer = mongoose.model("Customer",customerSchema);
+
+const addCustomer = async()=>{
+    let cust1 = new Customer({
+        name: "Rahul",
+    })
 }
+// const addOrders = async() => {
+//     let res = await Order.insertMany([
+//         { item: "Samosa", price: 12 } ,
+//         { item: "Chips", price: 10 },
+//         { item: "chocolate", price: 40 }
+//     ]);
+//     console.log(res);
+// }
+addOrders();
