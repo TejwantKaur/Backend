476811bb8b68b925d4c0cea19330a396d803949b
TejwantKaur <kaurtejwant998@gmail.com>
2024-01-26 22:33:50 +0530

    #

--- RelationShips/models/customer.js
+++ RelationShips/models/customer.js
@@ -8,3 +8,13 @@ main()
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
 }
+
+const Order = mongoose.model("Order",orderSchema);
+const addOrders = async() => {
+    let res = await Order.insertMany([
+        { item: "Samosa", price: 12 } ,
+        { item: "Chips", price: 10 },
+        { item: "chocolate", price: 40 }
+    ]);
+    console.log(res);
+}
